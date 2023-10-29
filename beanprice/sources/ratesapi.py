7f8a241d31890b6f6b0b95cbbe938aeeca273ad9
Max Smirnov <max.fleshy@gmail.com>
2023-10-29 13:29:51 +0400

    use apilayer.com instead of ratesapi ratesapi now redirects to apilayer.com see https://apilayer.com/marketplace/currency_data-api

--- beanprice/sources/ratesapi.py
+++ beanprice/sources/ratesapi.py
@@ -19,10 +19,10 @@ from decimal import Decimal
 import re
 import requests
 from dateutil.tz import tz
-from dateutil.parser import parse
+from dateutil.parser import parse as parse_date
+from datetime import datetime
 
 from beanprice import source
-
 
 class RatesApiError(ValueError):
     "An error from the Rates API."
@@ -41,31 +41,53 @@ def _parse_ticker(ticker):
             'Invalid ticker. Use "BASE-SYMBOL" format.')
     return match.groups()
 
-def _get_quote(ticker, date):
+def _get_latest_quote(ticker):
     """Fetch a exchangerate from ratesapi."""
     base, symbol = _parse_ticker(ticker)
-    params = {
-        'base': base,
-        'symbol': symbol,
-    }
-    response = requests.get(url='https://api.exchangerate.host/' + date, params=params)
 
+    params = {
+        'source': base,
+        'currencies': symbol,
+    }
+    
+    response = requests.get(url='https://api.apilayer.com/currency_data/live', params=params, headers={"apikey": "WF4hvihtwBxoZNlwvPZ4hDFK7nZfRYTm"})
+    
     if response.status_code != requests.codes.ok:
-        raise RatesApiError("Invalid response ({}): {}".format(response.status_code,
-                                                            response.text))
+        raise RatesApiError("Invalid response ({}): {}".format(response.status_code, response.text))
 
     result = response.json()
 
-    price = Decimal(str(result['rates'][symbol]))
-    time = parse(result['date']).replace(tzinfo=tz.tzutc())
+    price = Decimal(str(result['quotes'][base+symbol]))
+    time = datetime.fromtimestamp(result['timestamp']).replace(tzinfo=tz.tzutc())
 
     return source.SourcePrice(price, time, symbol)
 
+def _get_quote(ticker, date):
+    """Fetch a exchangerate from ratesapi."""
+    base, symbol = _parse_ticker(ticker)
+
+    params = {
+        'source': base,
+        'currencies': symbol,
+        'date': date
+    }
+
+    response = requests.get(url='https://api.apilayer.com/currency_data/historical', params=params, headers={"apikey": "WF4hvihtwBxoZNlwvPZ4hDFK7nZfRYTm"})
+    
+    if response.status_code != requests.codes.ok:
+        raise RatesApiError("Invalid response ({}): {}".format(response.status_code, response.text))
+
+    result = response.json()
+
+    price = Decimal(str(result['quotes'][base+symbol]))
+    time = parse_date(result['date']).replace(tzinfo=tz.tzutc())
+
+    return source.SourcePrice(price, time, symbol)
 
 class Source(source.Source):
 
     def get_latest_price(self, ticker):
-        return _get_quote(ticker, 'latest')
+        return _get_latest_quote(ticker)
 
     def get_historical_price(self, ticker, time):
         return _get_quote(ticker, time.date().isoformat())
